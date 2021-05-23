a63766e440d4c49f686d0e5f408414c74574f236
Luis Carrion <lcarrionalvarez@gmail.com>
2021-05-23 18:44:17 -0500

    Created kafka configuration

--- product-info/src/main/java/co/edu/umb/ds/productinfo/business/kafka/config/KafkaConfiguration.java
+++ product-info/src/main/java/co/edu/umb/ds/productinfo/business/kafka/config/KafkaConfiguration.java
@@ -15,5 +15,19 @@ import java.util.Map;
 
 @Configuration
 public class KafkaConfiguration {
+    @Bean
+    public ProducerFactory<String, KafkaProductInfoDto> producerFactory(){
+        Map<String, Object> config = new HashMap<>();
 
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, KafkaProductInfoDto> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
 }
