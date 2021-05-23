790feb0d2ba2e5a9cc0ed186b9e93c35b2c31646
leonardomn12 <69440096+leonardomn12@users.noreply.github.com>
2021-05-23 16:01:45 -0500

    Create Save Image

--- product-info/src/main/java/co/edu/umb/ds/productinfo/model/loader/CategoryLoader.java
+++ product-info/src/main/java/co/edu/umb/ds/productinfo/model/loader/CategoryLoader.java
@@ -9,5 +9,24 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CategoryLoader{
+public class CategoryLoader implements CommandLineRunner{
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Category categoryBook = new Category(CategoryName.LIBROS);
+        Category categoryTechnology = new Category(CategoryName.TECNOLOGIA);
+        Category categorySport = new Category(CategoryName.DEPORTE);
+        Category categoryVideoGame = new Category(CategoryName.VIDEOJUEGOS);
+        Category categoryPet = new Category(CategoryName.MASCOTAS);
+
+        categoryRepository.save(categoryBook);
+        categoryRepository.save(categoryTechnology);
+        categoryRepository.save(categorySport);
+        categoryRepository.save(categoryVideoGame);
+        categoryRepository.save(categoryPet);
+    }
 }
--- product-info/src/main/java/co/edu/umb/ds/productinfo/model/util/FileUtil.java
+++ product-info/src/main/java/co/edu/umb/ds/productinfo/model/util/FileUtil.java
@@ -10,6 +10,31 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileUtil {
+    public final static String UPLOADED_FOLDER = System.getProperty("user.home")
+            + File.separator + "productInfo" + File.separator;
 
+
+    public static String saveImageProductFile(MultipartFile file, String name) throws ApiRequestException {
+        try {
+            createFolder();
+            if (!file.isEmpty()) {
+                byte[] bytes = file.getBytes();
+                Path path = Paths.get(UPLOADED_FOLDER + name);
+                Files.write(path, bytes);
+                return UPLOADED_FOLDER + name;
+            }
+        } catch (IOException e) {
+            throw new ApiRequestException("Error al guardar la imagen");
+        }
+        throw new ApiRequestException("Hubo un error");
+    }
+
+
+    private static void createFolder() throws IOException {
+        Path folderDirectory = Paths.get(UPLOADED_FOLDER);
+        if (!Files.exists(folderDirectory)) {
+            Files.createDirectories(folderDirectory);
+        }
+    }
 
 }
